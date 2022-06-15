28555e11dbfe6a574cdeb93c2aae1ec16a8896cf
dzaki <dzakifauzi.mubarok96@gmail.com>
2022-06-16 00:00:32 +0700

    update loading

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,26 +2,29 @@ import React, { Component } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
+import Lottie from "lottie-react";
+
+import CinemaLoading from "../assets/animations/cinema_animation.json";
 import "../styles/App.css";
 
 class Homepage extends Component {
   state = {
     data: [],
-    // dataMovie: [],
-    // loading: true,
+    loading: true,
   };
   componentDidMount() {
-    this.fetchData();
+    setTimeout(() => {
+      this.fetchData();
+    }, 5000);
   }
 
   // simulasi pemanggilan
   fetchData() {
     axios
       .get(
-        "https://api.themoviedb.org/3/movie/now_playing?api_key=1fcd7008d5b1654db8f404670d2ed815&language=en-US&page=1"
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=1fcd7008d5b1654db8f404670d2ed815&language=en-US"
       )
       .then((res) => {
-        console.log(res);
         const { results } = res.data;
         this.setState({ data: results });
       })
@@ -31,15 +34,19 @@ class Homepage extends Component {
       .finally(() => this.setState({ loading: false }));
   }
   render() {
-    return (
-      <Layout>
-        <div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
-          {this.state.data.map((item) => (
-            <Card key={item.id} title={item.title} image={item.poster_path} />
-          ))}
-        </div>
-      </Layout>
-    );
+    if (this.state.loading) {
+      return <Lottie loop autoplay animationData={CinemaLoading} />;
+    } else {
+      return (
+        <Layout>
+          <div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
+            {this.state.data.map((item) => (
+              <Card key={item.id} title={item.title} image={item.poster_path} />
+            ))}
+          </div>
+        </Layout>
+      );
+    }
   }
 }
 
