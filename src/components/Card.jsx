d462d37dd182444ceaf466b53d336249203d484a
dzaki <dzakifauzi.mubarok96@gmail.com>
2022-06-17 21:10:42 +0700

    functional components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,17 +5,16 @@ export class Card extends Component {
   render() {
     return (
       <div className="container grow p-7 flex flex-col bg-blue-400 max-w-sm mx-auto rounded-md ">
-        <img
-          src={`https://image.tmdb.org/t/p/w500${this.props.image}`}
-          alt={this.props.image}
-          width="350"
-          height="450"
-          className="mx-auto"
-        />
-        <a href={"/movie/" + this.props.id} className="text-center text my-4">
-          {this.props.title}
-        </a>
-
+        <div onClick={this.props.onClickItem}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${this.props.image}`}
+            alt={this.props.image}
+            width="350"
+            height="450"
+            className="mx-auto"
+          />
+          <div className="text-center text my-4">{this.props.title}</div>
+        </div>
         <button className="bg-red-500 hover:bg-red-400 flex justify-center items-center p-2 space-x-5 mt-auto">
           <FaHeart /> <p>Add Favorite</p>
         </button>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,58 +1,67 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
 import Lottie from "lottie-react";
+import { withRouter } from "../utils/navigation";
+import { useNavigate } from "react-router-dom";
 
 import CinemaLoading from "../assets/animations/cinema_animation.json";
 import "../styles/App.css";
 
-class Homepage extends Component {
-  state = {
-    data: [],
-    loading: true,
-  };
-  componentDidMount() {
-    setTimeout(() => {
-      this.fetchData();
-    }, 2000);
-  }
+const Homepage = (props) => {
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // state = {
+  //   data: [],
+  //   loading: true,
+  // };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.fetchData();
+  //   }, 2000);
+  // }
 
   // simulasi pemanggilan
-  fetchData() {
+  function fetchData() {
     axios
       .get(
         "https://api.themoviedb.org/3/movie/now_playing?api_key=1fcd7008d5b1654db8f404670d2ed815&language=en-US"
       )
       .then((res) => {
         const { results } = res.data;
-        this.setState({ data: results });
+        // this.setState({ data: results });
+        // console.log(res.data);
+        setData(results);
       })
       .catch((err) => {
         console.log(err);
       })
-      .finally(() => this.setState({ loading: false }));
+      .finally(() => setLoading(false));
   }
-  render() {
-    if (this.state.loading) {
-      return <Lottie loop autoplay animationData={CinemaLoading} />;
-    } else {
-      return (
-        <Layout>
-          <div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
-            {this.state.data.map((item) => (
-              <Card
-                key={item.id}
-                id={item.id}
-                title={item.title}
-                image={item.poster_path}
-              />
-            ))}
-          </div>
-        </Layout>
-      );
-    }
-  }
-}
 
-export default Homepage;
+  if (loading) {
+    return <Lottie loop autoplay animationData={CinemaLoading} />;
+  } else {
+    return (
+      <Layout>
+        <div className="grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
+          {data.map((item) => (
+            <Card
+              key={item.id}
+              title={item.title}
+              image={item.poster_path}
+              onClickItem={() => navigate(`movie/${item.id}`)}
+            />
+          ))}
+        </div>
+      </Layout>
+    );
+  }
+};
+
+export default withRouter(Homepage);
